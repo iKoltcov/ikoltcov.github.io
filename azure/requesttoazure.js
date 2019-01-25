645a16b343be5da0bab0b74abcdd312fee59c09f
Ivan Koltcov <ikoltcov@yandex.ru>
2019-01-26 02:18:37 +0300

    master // add random button into azure test page

--- azure/requesttoazure.js
+++ azure/requesttoazure.js
@@ -7,7 +7,7 @@ $(document).ready(
                     type: 'POST',
                     data: JSON.stringify(
                         { 
-                            'Name': 'Azure' 
+                            'Name': 'some name' 
                         }),
                     beforeSend: function(request)
                     {
@@ -16,7 +16,32 @@ $(document).ready(
                     success: function(data)
                     {
                         $('.data-wrapper').show(500);
-                        $('.data').html(JSON.stringify(data));
+                        $('.data').html('Const: ' + JSON.stringify(data));
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        $('.data-wrapper').hide(500);
+                    }
+                });
+            }
+        )
+
+        $('.button_random').click(
+            function(){
+                $.ajax({
+                    url: 'https://ik-azuretest.azurewebsites.net/api/Function',
+                    type: 'POST',
+                    data: JSON.stringify(
+                        { 
+                            'Name': '' 
+                        }),
+                    beforeSend: function(request)
+                    {
+                        request.setRequestHeader('Content-Type', 'application/json');
+                    },
+                    success: function(data)
+                    {
+                        $('.data-wrapper').show(500);
+                        $('.data').html('Random: ' + JSON.stringify(data));
                     },
                     error: function(jqXHR, textStatus, errorThrown){
                         $('.data-wrapper').hide(500);
